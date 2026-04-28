a9a183af56d3bda5fe3d3a7a2238b9cf2ffe2bd0
raiselfg <idima6335@gmail.com>
2026-04-28 16:02:36 +0300

    fix: landing grid imgs optimization

--- apps/landing/features/landing/components/product-grid.tsx
+++ apps/landing/features/landing/components/product-grid.tsx
@@ -9,17 +9,17 @@ interface Props {
 export const ProductGrid = ({ products }: Props) => {
   return (
     <ul className="container grid list-none grid-cols-2 gap-2 p-0 sm:gap-4 md:grid-cols-3 md:gap-8 lg:grid-cols-3 lg:gap-12 xl:gap-16">
-      {products.map((product) => (
-        <li className="w-full" key={product.id}>
+      {products.map((product, index) => (
+        <li className="relative aspect-305/455 w-full" key={product.id}>
           <Image
             alt={product.name || 'Изображение товара'}
-            className="h-auto w-full border-2 border-[oklch(81%_0.18_77)] object-cover"
-            height={455}
-            loading="eager"
-            priority
-            sizes="(max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
+            className="border-2 border-[oklch(0.81_0.18_77)] object-cover transition-opacity"
+            fetchPriority={index === 0 ? 'high' : 'auto'}
+            fill
+            priority={index < 4}
+            quality={70}
+            sizes="(max-width: 768px) 50vw, (max-width: 1024px) 33vw, 400px"
             src={product.image}
-            width={305}
           />
         </li>
       ))}
