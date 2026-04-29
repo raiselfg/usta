8efda7ea713f0755cb6b392c75cfc8260d066cee
raiselfg <idima6335@gmail.com>
2026-04-29 23:56:13 +0300

    fix: landing api fetch in try catch, eslint config adapt to v10

--- apps/landing/features/landing/api/get-landing-data.ts
+++ apps/landing/features/landing/api/get-landing-data.ts
@@ -1,18 +1,22 @@
-import { LandingCategory } from '@usta/types/product-categories';
+import type { LandingCategory } from '@usta/types/product-categories';
 
 export async function getLandingData(): Promise<LandingCategory[]> {
   const baseUrl = process.env.NEXT_PUBLIC_API_URL;
   const url = `${baseUrl}/storefront/landing`;
 
-  const res = await fetch(url, {
-    next: {
-      tags: ['landing-data'],
-    },
-  });
+  try {
+    const res = await fetch(url, {
+      next: {
+        tags: ['landing-data'],
+      },
+    });
 
-  if (!res.ok) {
-    throw new Error(`Failed to fetch landing data: ${res.statusText}`);
+    if (!res.ok) {
+      throw new Error(`Failed to fetch landing data: ${res.statusText}`);
+    }
+
+    return res.json();
+  } catch {
+    return [{ id: '', name: '', product: [] }];
   }
-
-  return res.json();
 }
