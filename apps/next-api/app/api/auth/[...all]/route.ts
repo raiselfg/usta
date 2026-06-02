c26517ebd225a8565456ebecaa9a28e4e3a333a5
raiselfg <idima6335@gmail.com>
2026-06-02 15:51:49 +0300

    sadfv

--- apps/next-api/app/api/auth/[...all]/route.ts
+++ apps/next-api/app/api/auth/[...all]/route.ts
@@ -3,6 +3,5 @@ import { toNextJsHandler } from 'better-auth/next-js';
 import { auth } from '@/lib/auth';
 
 export const dynamic = 'force-dynamic';
-export const runtime = 'nodejs';
 
 export const { GET, POST } = toNextJsHandler(auth);
